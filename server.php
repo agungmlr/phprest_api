94d92063e4f8f0aa9469a5309391ebf1ea30b843
Mouhamad Hatta Hiroshi Sasmita <hiroshisasmita@gmail.com>
2017-10-22 18:58:47 +0700

    semua fungsi normal

--- server.php
+++ server.php
@@ -5,9 +5,7 @@
 	$password = "";
 	$database = "restoran_db";
 
-	//mysqli_connect($server, $username, $password, $database);
 	$mysqli = new mysqli($server, $username, $password, $database);
-	//mysql_select_db($database) or die ("<h1> Koneksi ke database Error : </h1>" . mysql_error());
 
 
 	@$operasi = $_GET['operasi'];
@@ -31,7 +29,7 @@
 			//rint(count($params));
 
 			for ($i=0; $i < count($params); $i++) { 
-				$query = "insert into makanan values('".$params[$i]['id_makanan']."','".$params[$i]['nama']."','".$params[$i]['desc']."','".$params[$i]['harga_beli']."','".$params[$i]['harga_jual']."') ";
+				$query = "insert into makanan values('".$params[$i]['id_makanan']."','".$params[$i]['nama']."','".$params[$i]['deskripsi']."','".$params[$i]['harga_beli']."','".$params[$i]['harga_jual']."') ";
 
 				if ($result = mysqli_query($mysqli,$query)){
 					print("Seccess!");
@@ -45,7 +43,6 @@
 
 			$result = mysqli_query($mysqli, "SELECT * FROM makanan WHERE id_makanan = '$id'");
 			$data_array = array();
-			//$data_array = mysqli_fetch_assoc($result);
 
 			while ($data = mysqli_fetch_assoc($result)){
 				$data_array[] = $data;
@@ -56,6 +53,23 @@
 			break;
 
 		case "update" :
+			@$id = $_GET['id'];
+
+
+			$json_string = file_get_contents('php://input');
+			
+			$params = array();
+			$params = json_decode($json_string,true);
+
+			$query = "UPDATE makanan SET nama = '".$params['nama']."' , deskripsi =  '".$params['deskripsi']."' , harga_beli = 
+			'".$params['harga_beli']."' , harga_jual =  '".$params['harga_jual']."' WHERE id_makanan = '$id'";
+
+
+			if ($result = mysqli_query($mysqli,$query)){
+				print("Seccess Update!");
+			} else {
+				echo mysqli_error($mysqli);
+			}
 			
 			
 			break;
@@ -76,20 +90,5 @@
 			break;
 	}
 
-	class makanan{
-		public $id;
-		public $nama;
-		public $desc;
-		public $harga_beli;
-		public $harga_jual;
-
-		function __construct($id, $nama, $desc, $harga_beli, $harga_jual){
-			$this->id = $id;
-			$this->nama = $nama;
-			$this->desc = $desc;
-			$this->harga_beli = $harga_beli;
-			$this->harga_jual = $harga_jual;
-		}
-	}
 
 ?>
